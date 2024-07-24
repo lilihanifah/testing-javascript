8a43e60b7deaf3a974ae5ee19d03f210f46b4edb
lilihanifah <lilihanifah30.lh@gmail.com>
2024-07-24 09:56:01 +0700

    update commit

---
+++ JS/switch.js
@@ -0,0 +1,28 @@
+const hari = "7";
+
+switch (hari){
+    case "0":
+        console.log("Hari Minggu");
+        break;
+    case "1":
+        console.log("Hari Senin");
+        break;
+    case "2":
+        console.log("Hari Selasa");
+        break;
+    case "3":
+        console.log("Hari Rabu");
+        break;
+    case "4":
+        console.log("Hari Kamis");
+        break;
+    case "5":
+        console.log("Hari Jumat");
+        break;
+    case "6":
+        console.log("Hari Sabtu");
+        break;
+    default:
+        console.log("Maaf, hanya dapat diinput angka 0-6");
+        break;
+}
